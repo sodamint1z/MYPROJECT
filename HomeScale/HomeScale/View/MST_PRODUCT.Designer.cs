e5ae603c93527daa702fb5dc4661508c8500ff4e
sodamint1z <snowmonkey_89@hotmail.com>
2017-12-09 00:05:46 +0700

    9/12/2017

--- HomeScale/HomeScale/View/MST_PRODUCT.Designer.cs
+++ HomeScale/HomeScale/View/MST_PRODUCT.Designer.cs
@@ -32,6 +32,14 @@
             this.lblProductUnit = new System.Windows.Forms.Label();
             this.txtProductName = new System.Windows.Forms.TextBox();
             this.txtProductUnit = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // lblProductName
@@ -39,7 +47,7 @@
             this.lblProductName.AutoSize = true;
             this.lblProductName.BackColor = System.Drawing.SystemColors.Control;
             this.lblProductName.Font = new System.Drawing.Font("Verdana", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblProductName.Location = new System.Drawing.Point(65, 139);
+            this.lblProductName.Location = new System.Drawing.Point(61, 215);
             this.lblProductName.Name = "lblProductName";
             this.lblProductName.Size = new System.Drawing.Size(93, 29);
             this.lblProductName.TabIndex = 0;
@@ -50,7 +58,7 @@
             this.lblProductUnit.AutoSize = true;
             this.lblProductUnit.BackColor = System.Drawing.SystemColors.Control;
             this.lblProductUnit.Font = new System.Drawing.Font("Verdana", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblProductUnit.Location = new System.Drawing.Point(495, 139);
+            this.lblProductUnit.Location = new System.Drawing.Point(491, 215);
             this.lblProductUnit.Name = "lblProductUnit";
             this.lblProductUnit.Size = new System.Drawing.Size(114, 29);
             this.lblProductUnit.TabIndex = 1;
@@ -60,7 +68,7 @@
             // 
             this.txtProductName.BackColor = System.Drawing.SystemColors.Window;
             this.txtProductName.Font = new System.Drawing.Font("Verdana", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtProductName.Location = new System.Drawing.Point(174, 136);
+            this.txtProductName.Location = new System.Drawing.Point(170, 212);
             this.txtProductName.Multiline = true;
             this.txtProductName.Name = "txtProductName";
             this.txtProductName.Size = new System.Drawing.Size(300, 40);
@@ -69,11 +77,85 @@
             // txtProductUnit
             // 
             this.txtProductUnit.Font = new System.Drawing.Font("Verdana", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtProductUnit.Location = new System.Drawing.Point(625, 136);
+            this.txtProductUnit.Location = new System.Drawing.Point(621, 212);
             this.txtProductUnit.Multiline = true;
             this.txtProductUnit.Name = "txtProductUnit";
             this.txtProductUnit.Size = new System.Drawing.Size(300, 40);
             this.txtProductUnit.TabIndex = 3;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnAdd.Font = new System.Drawing.Font("Verdana", 18F);
+            this.btnAdd.Location = new System.Drawing.Point(4, 111);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(150, 80);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "เพิ่ม";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnEdit.Font = new System.Drawing.Font("Verdana", 18F);
+            this.btnEdit.Location = new System.Drawing.Point(174, 111);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(150, 80);
+            this.btnEdit.TabIndex = 5;
+            this.btnEdit.Text = "แก้ไข";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnDelete.Font = new System.Drawing.Font("Verdana", 18F);
+            this.btnDelete.Location = new System.Drawing.Point(344, 111);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(150, 80);
+            this.btnDelete.TabIndex = 6;
+            this.btnDelete.Text = "ลบ";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnCancel.Font = new System.Drawing.Font("Verdana", 18F);
+            this.btnCancel.Location = new System.Drawing.Point(514, 111);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(150, 80);
+            this.btnCancel.TabIndex = 7;
+            this.btnCancel.Text = "ยกเลิก";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnSave.Font = new System.Drawing.Font("Verdana", 18F);
+            this.btnSave.Location = new System.Drawing.Point(684, 111);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(150, 80);
+            this.btnSave.TabIndex = 8;
+            this.btnSave.Text = "บันทึก";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnBack
+            // 
+            this.btnBack.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnBack.Font = new System.Drawing.Font("Verdana", 18F);
+            this.btnBack.Location = new System.Drawing.Point(854, 111);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(150, 80);
+            this.btnBack.TabIndex = 9;
+            this.btnBack.Text = "ย้อนกลับ";
+            this.btnBack.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(66, 268);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(855, 422);
+            this.dataGridView1.TabIndex = 10;
             // 
             // MST_PRODUCT
             // 
@@ -81,6 +163,13 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDark;
             this.ClientSize = new System.Drawing.Size(1008, 729);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnAdd);
             this.Controls.Add(this.txtProductUnit);
             this.Controls.Add(this.txtProductName);
             this.Controls.Add(this.lblProductUnit);
@@ -88,6 +177,7 @@
             this.Name = "MST_PRODUCT";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "HomeScale";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -99,5 +189,12 @@
         private System.Windows.Forms.Label lblProductUnit;
         private System.Windows.Forms.TextBox txtProductName;
         private System.Windows.Forms.TextBox txtProductUnit;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
